48fb8f69565558ad4025fe8a6eb2d10fb244d65d
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-03-07 17:14:48 +0700

    Initial Commit

---
+++ learn-swiftUI/learn_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftUIApp.swift
+//  learn-swiftUI
+//
+//  Created by Ricky Primayuda Putra on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
